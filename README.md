da68f96ae1a96bceec25685afda7f94d6edce1ec
chun <chun.xiao@uts.edu.au>
2021-08-13 22:22:52 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# data_vis_app
+
+This is a streamlit for data visualization purpose.
---
+++ app_data_vis.py
@@ -0,0 +1,19 @@
+"""
+Created on Fri Aug 13 21:48:35 2021
+
+@author: Chun
+"""
+import streamlit as st
+
+import pandas as pd
+import numpy as np
+
+
+st.title("Chun's Data Visualization app")
+
+
+upload_file = st.file_uploader('', type="csv", accept_multiple_files=False)
+
+if upload_file:
+    file_df = pd.read_csv(upload_file)
+    st.write(file_df.head())
